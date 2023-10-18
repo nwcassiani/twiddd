ebf221029ced922e1526ccc0a00562cc00d03252
Nathan Cassiani <nwcassiani@gmail.com>
2023-10-18 16:58:12 -0500

    add timestamp

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 
 $(document).ready(() => {
   const $body = $('body');
-  $body.html('');
+  $body.html(''); // clear the body
 
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
+  const $div = $('<div></div>');
+  $body.append($div);
+  // $div.html(''); // clear the div
+  // make tweets a function that returns a map
+  const $tweets = function() {
+    return streams.home.map((tweet) => {
+      const $tweet = $('<div></div>');
+      $tweet.attr('class', 'old-tweets');
+      const text = `@${tweet.user}: ${tweet.message}`;
 
-    $tweet.text(text);
+      $tweet.text(text);
+      let time = new Date().getTime();
+      $tweet.append(time);
 
-    return $tweet;
+      return $div.prepend($tweet);
+    });
+  };
+
+  const $button = $('<button></button>');
+  $body.prepend($button);
+  $button.text('Gimme twidds');
+  $button.on('click', function() {
+    // remove here
+    $('.old-tweets').remove();
+    $tweets();
+    new Date().getTime();
   });
-  $body.append($tweets);
+ 
+  // $body.append($tweets); // change from adding an entire array to just adding individual tweets
+  // $div.append($tweets);
+  // put inside map function?
+  // instead of adding to body, you will want to make a div to hold tweets, so you can clear div instead of entire body
 
 });
