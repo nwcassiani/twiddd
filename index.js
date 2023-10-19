ab91cd59869ce2fdd4f3a4573071ee71d91e69ff
Nathan Cassiani <nwcassiani@gmail.com>
2023-10-19 16:53:27 -0500

    moment stamp

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 $(document).ready(() => {
+  // current time
+  var now = moment();
+
   const $body = $('body');
   $body.html(''); // clear the body
 
@@ -18,7 +21,7 @@ $(document).ready(() => {
 
       // const text = `@${username}: ${tweet.message}`;
       const text = `${tweet.message}`;
-      const un = `${tweet.user}`;
+      const un = `@${tweet.user}`;
 
       $tweet.text(text);
       $username.text(un);
@@ -27,12 +30,34 @@ $(document).ready(() => {
       // css display inline-block, add later when styling
       $tweet.prepend($username);
 
-      let time = new Date().getTime();
-      $tweet.append(time);
+      // let time = new Date().getTime();
+      // let time = moment().seconds();
+      // let $time = $('<time></time>');
+      // let date = new Date();
+      // let time = moment(date).fromNow();
+      // let time = ` ${moment().diff(now, 'seconds')} seconds ago`;
+
+      // $tweet.append(time);
 
       // create variable for username, add click function
+      // will i need a separate variable for each user?
       $username.on('click', function() {
         // what to do here so only users tweets show
+        // remove other users usernames and tweets
+        $('.old-tweets').remove();
+        // will prepend just the tweet clicked on
+        // for loop through tweet.user array
+        for (let i = 0; i < streams.users[tweet.user].length; i++ ) { 
+          const $usertweet = $('<div></div>');
+          $usertweet.attr('class', 'users-tweets');
+          $usertweet.text(streams.users[tweet.user][i].message);
+          $usertweet.prepend($username);
+          // $usertweet.append(time);
+          $div.prepend($usertweet);
+          // create a variable for each tweet, so will log a list instead of one long string
+        }
+        // $div.prepend($tweet);
+        // make a jquery variable for streams.users[tweet.message]?
         console.log(streams.users[tweet.user]);
       });
 
@@ -46,8 +71,14 @@ $(document).ready(() => {
   $button.on('click', function() {
     // remove here
     $('.old-tweets').remove();
+    $('.users-tweets').remove();
+    // where to append moment call?
+    // $div.append(` ${moment().diff(now, 'seconds')} seconds ago`);
     $tweets();
-    new Date().getTime();
+    $('.old-tweets').append(` ${moment().diff(now, 'seconds')} seconds ago`);
+    // new Date();
+    // moment(date).fromNow();
+    
   });
   // add click to tweet, inside of tweets function?
   // or change tweets function, so each individual tweet can be accessed?
