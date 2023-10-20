2a2033832706dee84fd063d3aa0842da41c6da41
Nathan Cassiani <nwcassiani@gmail.com>
2023-10-20 17:49:53 -0500

    postDiv

--- index.js
+++ index.js
@@ -25,16 +25,24 @@ $(document).ready(() => {
 
       $tweet.text(text);
       $username.text(un);
+      // make username bold
+      $username.css('font-weight', 'bold');
       // prepend $username to $tweet, will show up on different lines
       // figure out how to have username on one line and tweet next line
       // css display inline-block, add later when styling
-      $tweet.prepend($username);
+      
 
       // time variable 
+      const $time = $('<span>');
       let time = ` ${moment(tweet.created_at).fromNow()}`;
+      $time.text(time);
+      $time.css('color', 'gray');
       // console.log(typeof time);
       // append time to $tweet
-      $tweet.append(time);
+      $username.append($time);
+      $tweet.prepend($username);
+      // $tweet.append($time);
+      $tweet.css('padding', '2');
 
       // create variable for username, add click function
       $username.on('click', function() {
@@ -46,21 +54,31 @@ $(document).ready(() => {
           const $usertweet = $('<div></div>');
           $usertweet.attr('class', 'users-tweets');
           $usertweet.text(streams.users[tweet.user][i].message);
+          // $usertweet.append(time);
           $usertweet.prepend($username);
-          $usertweet.append(time);
+          $usertweet.css('padding', '2');
           $div.prepend($usertweet);
         }
 
-        console.log(streams.users[tweet.user]);
+        // console.log(streams.users[tweet.user]);
       });
 
       return $div.prepend($tweet);
     });
   };
+  // may need to put button inside of div to align properly
+  // tweet button div
+  const $postDiv = $('<div>');
+  $postDiv.attr('id', 'twidd-buttons');
+  $body.prepend($postDiv);
+  $postDiv.css('margin-left', 'auto').css('margin-right', 'auto').css('width', '50em');
+  $postDiv.css('padding', '10px');
 
   const $button = $('<button></button>');
-  $body.prepend($button);
-  $button.text('Gimme twidds');
+  $postDiv.append($button);
+  $button.text('gimme twidds');
+  $button.css('padding', '2');
+  $button.css('margin-left', '10px');
   $button.on('click', function() {
     // remove here
     $('.old-tweets').remove();
@@ -80,7 +98,7 @@ $(document).ready(() => {
   const $divun = $('<div>');
   $divun.attr('class', 'user-name');
   const $label = $('<label></label>');
-  $label.attr('for', 'name').text('Name');
+  $label.attr('for', 'name').text('name');
   $label.append($('<input>').attr('type', 'text').attr('id', 'name'));
   $divun.append($label);
   // append divun to form
@@ -88,14 +106,23 @@ $(document).ready(() => {
   
   const $divmsg = $('<div></div>');
   $divmsg.attr('class', 'message');
-  $divmsg.append($('<label>').attr('for', 'msg').text('Message'));
+  $divmsg.append($('<label>').attr('for', 'msg').text('twidd'));
   $divmsg.append($('<textarea>').attr('id', 'msg'));
   // append to form 
   $form.append($divmsg);
   // send twidd button
-  $form.append($('<button>').attr('type', 'button').attr('id', 'sendtwidd').text('Send Twidd'));
+  $form.append($('<button>').attr('type', 'button').attr('id', 'sendtwidd').text('send twidd'));
 
-  $body.prepend($form);
+  // FORM STYLING
+  $form.css('margin', '10px');
+  $form.css('width', '100px');
+  $form.css('padding', '2');
+  // $form.css('border', '1pm solid #CCC').css('border-radius', '1');
+
+  $('<textarea>').css('vertical-align', 'top');
+
+
+  $postDiv.prepend($form);
   // on button click, post tweet and sending data to streams? or make new similar object for user
   // const userStream = {
   //   home: [],
@@ -110,6 +137,9 @@ $(document).ready(() => {
     streams.users[window.visitor] = [];
     console.log(streams.users);
     writeTweet($umsg);
+    $('.old-tweets').remove();
+    $('.users-tweets').remove();
+    $tweets();
     // $('#timeline').prepend(streams.users[window.visitor].slice(0, 1));
   });
   
@@ -117,5 +147,25 @@ $(document).ready(() => {
 
   // put inside map function?
   // instead of adding to body, you will want to make a div to hold tweets, so you can clear div instead of entire body
+  // STYLE
+  $('body').css('background-color', 'rgb(255, 236, 179)');
+  $('body').css('font-family', 'Arial');
+
+  // Header
+  const $header = $('<header>');
+  const $h1 = $('<h1>');
+  $h1.text('twiddler');
+  $h1.css('text-align', 'center');
+  $body.prepend($header);
+  $header.prepend($h1);
+  $header.css('border', '2px solid black');
+  $header.css('margin-left', 'auto').css('margin-right', 'auto').css('width', '10em');
+
+  // twidds
+  $('button').css('font-weight', 'bold');
+
+  //TIMELINE
+  $div.css('background-color', 'rgb(255, 242, 204)');
+  $div.css('margin-left', 'auto').css('margin-right', 'auto').css('width', '50em');
 
 });
